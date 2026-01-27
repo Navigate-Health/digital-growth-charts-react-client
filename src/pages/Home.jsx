7d91e7a50ab253279dcb903396c53d7a69c4da09
Thomas Porteus <1842033+tporteus@users.noreply.github.com>
2026-01-27 22:25:40 +0000

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,6 @@ const Home = () => {
   return (
     <div>
       <MeasurementSegment />
-      <small><i>RCPCH digital Growth Charts - Client : v{packageJson.version}, Charts: v{packageJson.dependencies["@rcpch/digital-growth-charts-react-component-library"]}.</i></small>
     </div>
   );
 };
